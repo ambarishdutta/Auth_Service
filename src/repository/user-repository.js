7c527b1989d563f64bfeb42a7882197f7cd02d46
Ambarish <ambarish.dutta1010@gmail.com>
2023-03-18 00:42:10 +0530

    Adding client errors file

--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -1,5 +1,7 @@
 const { User, Role } = require('../models/index');
+const ClientError = require('../utils/client-error');
 const ValidationError = require('../utils/validation-error');
+const { StatusCodes } = require('http-status-codes');
 
 class UserRepository {
 
@@ -50,6 +52,14 @@ class UserRepository {
                     email: userEmail
                 }
             });
+            if(!user) {
+                throw new ClientError(
+                    'AttributeNotFound',
+                    'Invalid email sent in the request',
+                    'Please check the email, as there is no record of the email',
+                    StatusCodes.NOT_FOUND
+                );
+            }
             return user;
         } catch (error) {
             console.log("Something went wrong in the repository layer");
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -37,6 +37,9 @@ class UserService {
             return newJWT;
 
         } catch (error) {
+            if(error.name == 'AttributeNotFound') {
+                throw error;
+            }
             console.log("Something went wrong in the sign in process");
             throw error;
         }
